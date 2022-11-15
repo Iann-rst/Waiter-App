8e8d876eb1171a37a2ac69cf1a03088f2a85bd63
Iann-rst <iannrst11@gmail.com>
2022-11-15 18:18:53 -0300

    feat: Estabelecer conexão com o mongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
-const port = 3002;
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}/`);
+
+mongoose.connect('mongodb://localhost:27017').then(() => {
+  console.log('Connected to MongoDB');
+
+  const port = 3002;
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/`);
+  });
+
+}).catch(() => {
+  console.log('Error ao conectar com o MongoDB');
 });
+
