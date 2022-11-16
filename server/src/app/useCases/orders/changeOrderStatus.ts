058b211f5522e333130ca3a9c97d6c4d24294805
Iann-rst <iannrst11@gmail.com>
2022-11-16 15:13:27 -0300

    feat: Trocar o status de um pedido

---
+++ server/src/app/useCases/orders/changeOrderStatus.ts
@@ -0,0 +1,23 @@
+import { Order } from './../../models/Order';
+import { Request, Response } from 'express';
+
+export async function changeOrderStatus(req: Request, res: Response) {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+      return res.status(400).json({
+        error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE.'
+      });
+    }
+
+    await Order.findByIdAndUpdate(orderId, { status });
+
+    res.sendStatus(204);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+}
