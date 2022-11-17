69b317c5ff42f81b089602e2994f12e6801239d2
Iann-rst <iannrst11@gmail.com>
2022-11-17 15:51:46 -0300

    instalação e configuração tailwindcss

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type
+ {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+};
