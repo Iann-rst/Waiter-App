942282330927c69b262d3a17f856d6e12e00332f
Iann-rst <iannrst11@gmail.com>
2022-11-17 15:52:11 -0300

    feat: Componente Header

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import logo from '../assets/image/logo.svg';
+
+export default function Header() {
+  return (
+    <header className='bg-[#D73035] flex justify-center h-48'>
+      <div className='flex items-center justify-between w-full max-w-[1216px]'>
+        <div>
+          <h1 className='font-semibold text-4xl text-white'>Pedidos</h1>
+          <h2 className='font-normal text-base text-white opacity-90'>Acompanhe os pedidos dos clientes</h2>
+        </div>
+        <img src={logo} alt="" />
+      </div>
+    </header>
+  );
+}
