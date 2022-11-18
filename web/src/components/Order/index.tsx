5f25d2bf6f1417a2288c629a6536d3b438dc6033
Iann-rst <iannrst11@gmail.com>
2022-11-18 13:03:18 -0300

    feat: propriedades do pedido

--- web/src/components/Order/index.tsx
+++ web/src/components/Order/index.tsx
@@ -1,11 +1,51 @@
+import { OrderProps } from '../../@types/Order';
 import { OrderBoard } from '../OrderBoard';
 
 export function Order() {
+
+  const orders: OrderProps[] = [
+    {
+      '_id': '63750c1da59520df0875b3b1',
+      'table': '4',
+      'status': 'WAITING',
+      'products': [
+        {
+          'product': {
+            'name': 'Hambúrguer de Frango',
+            'imagePath': '1668613049309-chicken.png',
+            'price': 85,
+          },
+          'quantity': 5,
+          '_id': '63750c1da59520df0875b3b2'
+        },
+        {
+          'product': {
+            'name': 'Coca cola',
+            'imagePath': '1668613283068-coca-cola.png',
+            'price': 12,
+          },
+          'quantity': 4,
+          '_id': '63750c1da59520df0875b3b3'
+        },
+        {
+          'product': {
+
+            'name': 'Suco de Laranja',
+            'imagePath': '1668613323489-suco-de-laranja.png',
+            'price': 7,
+          },
+          'quantity': 1,
+          '_id': '63750c1da59520df0875b3b4'
+        }
+      ]
+    }
+  ];
+
   return (
     <main className="w-full max-w-[1216px] my-10 mx-auto flex gap-8">
-      <OrderBoard />
-      <OrderBoard />
-      <OrderBoard />
+      <OrderBoard icon='🕑' title="Fila de espera" orders={orders} key={orders[0]._id} />
+      <OrderBoard icon="👩‍🍳" title="Em produção" orders={[]} />
+      <OrderBoard icon="✅" title="Pronto!" orders={[]} />
     </main>
   );
 }
--- web/src/components/OrderBoard/index.tsx
+++ web/src/components/OrderBoard/index.tsx
@@ -1,18 +1,34 @@
-export function OrderBoard() {
+import { OrderProps } from '../../@types/Order';
+
+interface OrderBoardProps {
+  icon: string;
+  title: string;
+  orders: OrderProps[];
+}
+
+export function OrderBoard({ icon, title, orders }: OrderBoardProps) {
   return (
     <div className="p-4 border rounded-2xl flex flex-col items-center flex-1">
       <header className="p-2 flex gap-2 text-sm items-center text-[#333]">
-        <span>🕑</span>
-        <strong>Fila de espera</strong>
-        <span>(1)</span>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders.length})</span>
       </header>
 
-      <div className="flex flex-col w-full mt-6">
-        <button className="border h-32 bg-white rounded-lg flex flex-col items-center justify-center gap-1 mb-6 hover:cursor-pointer">
-          <strong className="font-medium text-[#333] text-base">Mesa 2</strong>
-          <span className="font-normal text-sm text-[#666]">2 itens</span>
-        </button>
+      {orders.length > 0 && <div className="flex flex-col w-full mt-6">
+        {orders.map((order) => (
+          <button key={order._id} className="border h-32 bg-white rounded-lg flex flex-col items-center justify-center gap-1 mb-6 hover:cursor-pointer">
+            <strong className="font-medium text-[#333] text-base">
+              Mesa {order.table}
+            </strong>
+            <span className="font-normal text-sm text-[#666]">
+              {order.products.length} items
+            </span>
+          </button>
+        ))}
       </div>
+      }
+
     </div>
   );
 }
