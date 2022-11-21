61e709aee2ca593bbdb758843978c4cf9d7ca4ec
Iann-rst <iannrst11@gmail.com>
2022-11-21 16:29:20 -0300

    feat: Componente de Categorias

---
+++ waiterMobile/src/components/Categories/index.tsx
@@ -0,0 +1,39 @@
+import { Text } from '../Text';
+import { FlatList } from 'react-native';
+
+import { categories } from '../../mocks/categories';
+
+import * as S from './styles';
+import { useState } from 'react';
+
+export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
+  return (
+    <FlatList
+      horizontal
+      data={categories}
+      keyExtractor={(category) => category._id}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <S.Category onPress={() => handleSelectCategory(category._id)}>
+            <S.Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </S.Icon>
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>{category.name}</Text>
+          </S.Category>
+        );
+      }}
+      contentContainerStyle={{ paddingRight: 24 }}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+}
