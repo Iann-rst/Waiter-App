ebd57fbff0c3b965e7a49736de4c755833c283aa
Iann-rst <iannrst11@gmail.com>
2022-11-21 15:31:52 -0300

    feat: Screen Main

---
+++ waiterMobile/src/Main/index.tsx
@@ -0,0 +1,27 @@
+import { Header } from '../components/Header';
+
+import * as S from './styles';
+
+export function Main() {
+  return (
+    <>
+      <S.Container>
+        <Header />
+        <S.CategoriesContainer>
+
+        </S.CategoriesContainer>
+
+        <S.MenuContainer>
+
+        </S.MenuContainer>
+
+
+      </S.Container>
+
+      <S.Footer>
+        <S.FooterContainer></S.FooterContainer>
+      </S.Footer>
+    </>
+
+  );
+}
---
+++ waiterMobile/src/Main/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+import { Platform, StatusBar } from 'react-native';
+
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  flex: 1;
+  background: #fafafa;
+`;
+
+export const CategoriesContainer = styled.View`
+  height: 73px;
+  background: red;
+  margin-top: 34px;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+`;
+
+export const FooterContainer = styled.SafeAreaView`
+`;
+
