5c4c2227062d4b54072bdf8468b01d2e10526307
Iann-rst <iannrst11@gmail.com>
2022-11-21 18:14:32 -0300

    fix: mocks products

--- waiterMobile/src/mocks/products.ts
+++ waiterMobile/src/mocks/products.ts
@@ -1,9 +1,9 @@
-const products = [
+export const products = [
   {
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: '1668609338794-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
@@ -32,7 +32,7 @@ const products = [
     _id: '6372e276a381106c0f854cb3',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
+    imagePath: '1668613283068-coca-cola.png',
     price: 7,
     ingredients: [],
   },
