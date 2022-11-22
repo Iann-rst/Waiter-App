86705b50143a0473860cdf1ac148d5a9f03a61e2
Iann-rst <iannrst11@gmail.com>
2022-11-22 12:51:50 -0300

    feat: add modal detalhe do produto

--- waiterMobile/src/components/Menu/index.tsx
+++ waiterMobile/src/components/Menu/index.tsx
@@ -1,38 +1,59 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { Product } from '../../@types/product';
 
 import { products } from '../../mocks/products';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductDetailsModal } from '../ProductDetailsModal';
 import { Text } from '../Text';
 
 import * as S from './styles';
 
 export function Menu() {
+  const [modal, setModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
+
+  function handleOpenModal(product: Product) {
+    setModal(true);
+    setSelectedProduct(product);
+  }
+
   return (
-    <FlatList
-      data={products}
-      keyExtractor={product => product._id}
-      renderItem={({ item: product }) => (
-        <S.Product>
-          <S.ProductImage
-            source={{ uri: `http://10.0.0.168:3002/uploads/${product.imagePath}`, }}
-          />
+    <>
 
-          <S.ProductDetails>
-            <Text size={16} weight="600">{product.name}</Text>
-            <Text size={14} weight="400" color="#666" style={{ marginVertical: 8 }}>{product.description}</Text>
-            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
-          </S.ProductDetails>
+      <ProductDetailsModal
+        onClose={() => setModal(false)}
+        visible={modal}
+        product={selectedProduct}
+      />
 
-          <S.AddToCartButton>
-            <PlusCircle />
-          </S.AddToCartButton>
-        </S.Product>
-      )}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      ItemSeparatorComponent={S.Separator}
-    />
+
+      <FlatList
+        data={products}
+        keyExtractor={product => product._id}
+        renderItem={({ item: product }) => (
+          <S.Product onPress={() => handleOpenModal(product)}>
+            <S.ProductImage
+              source={{ uri: `http://10.0.0.168:3002/uploads/${product.imagePath}`, }}
+            />
+
+            <S.ProductDetails>
+              <Text size={16} weight="600">{product.name}</Text>
+              <Text size={14} weight="400" color="#666" style={{ marginVertical: 8 }}>{product.description}</Text>
+              <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
+            </S.ProductDetails>
+
+            <S.AddToCartButton>
+              <PlusCircle />
+            </S.AddToCartButton>
+          </S.Product>
+        )}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        ItemSeparatorComponent={S.Separator}
+      />
+    </>
   );
 }
 
