a59d4f8eb86bac93772f84b0727a640e8e43e601
Iann-rst <iannrst11@gmail.com>
2022-11-22 11:03:39 -0300

    feat: Header do pedido

--- waiterMobile/src/components/Header/index.tsx
+++ waiterMobile/src/components/Header/index.tsx
@@ -1,12 +1,37 @@
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
 import * as S from './styles';
 
+interface HeaderProps {
+  selectedTable?: string;
+  onCancelOrder: () => void;
+}
 
-export function Header() {
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <S.Container>
-      <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
-      <Text size={24} weight="700">WAITER<Text size={24}>APP</Text></Text>
+      {!selectedTable && (
+        <>
+
+          <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
+          <Text size={24} weight="700">WAITER<Text size={24}>APP</Text></Text>
+        </>
+      )}
+
+      {selectedTable && (
+        <S.OrderHeader>
+          <S.ContentHeader>
+            <Text weight='600' size={24}>Pedido</Text>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <Text weight='600' color='#d73035' size={14}>Cancelar Pedido</Text>
+            </TouchableOpacity>
+          </S.ContentHeader>
+
+          <S.Table>
+            <Text color="#666666" size={16} weight="400">Mesa {selectedTable}</Text>
+          </S.Table>
+        </S.OrderHeader>
+      )}
     </S.Container>
   );
 }
--- waiterMobile/src/components/Header/styles.ts
+++ waiterMobile/src/components/Header/styles.ts
@@ -3,3 +3,22 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   margin: 24px 24px 0;
 `;
+
+export const OrderHeader = styled.View`
+
+`;
+
+export const ContentHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Table = styled.View`
+  width: 100%;
+  padding: 16px;
+  margin-top: 24px;
+  background: #FFFFFF;
+  border: 1px solid rgba(204, 204, 204, 0.3);
+  border-radius: 8px;
+`;
