19d43d232cd8df3894bd62e13517f1231305b0b0
Iann-rst <iannrst11@gmail.com>
2022-11-22 15:09:19 -0300

    feat: add carrinho de itens

--- waiterMobile/src/Main/index.tsx
+++ waiterMobile/src/Main/index.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
+import { CartItemProps } from '../@types/cartItem';
 import { Button } from '../components/Button';
+import { Cart } from '../components/Cart';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu/';
 import { TableModal } from '../components/TableModal';
+import { products } from '../mocks/products';
 
 import * as S from './styles';
 
 export function Main() {
   const [tableModalVisible, setTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
+
+  const [cartItem, setCartItem] = useState<CartItemProps[]>([
+    {
+      quantity: 1,
+      product: products[0]
+    },
+    {
+      quantity: 2,
+      product: products[1]
+    }
+  ]);
 
 
   function handleSaveTable(table: string) {
@@ -36,11 +50,15 @@ export function Main() {
 
       <S.Footer>
         <S.FooterContainer>
-          {!selectedTable &&
+          {!selectedTable && (
             <Button onPress={() => setTableModalVisible(true)}>
               Novo Pedido
             </Button>
-          }
+          )}
+
+          {selectedTable && (
+            <Cart cartItems={cartItem} />
+          )}
         </S.FooterContainer>
       </S.Footer>
 
